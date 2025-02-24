a6dc3f0923e8e46edfcabf533fe8b80dce769d38
DevangT98 <devangjthakkar@gmail.com>
2025-02-23 19:23:23 -0500

    set-up airflow dag and add spark and cassandra in docker-compose.yml

--- dags/kafka_stream.py
+++ dags/kafka_stream.py
@@ -41,14 +41,23 @@ def stream_data():
     import json
     from kafka import KafkaProducer
     import time
+    import logging
 
-    res = get_data()
-    res = format_data(res)
     # print(json.dumps(res, indent=3))  # print formatted json
 
-    producer = KafkaProducer(bootstrap_servers=["localhost:9092"], max_block_ms=5000)
+    producer = KafkaProducer(bootstrap_servers=["broker:29092"], max_block_ms=5000)
+    curr_time = time.time()
 
-    producer.send("users_created", json.dumps(res).encode("utf-8"))
+    while True:
+        if time.time() > curr_time + 60:
+            break
+        try:
+            res = get_data()
+            res = format_data(res)
+            producer.send("users_created", json.dumps(res).encode("utf-8"))
+        except Exception as e:
+            logging.error(f"Error: {e}")
+            continue
 
 
 with DAG(
